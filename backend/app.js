7dd0453c7284fd87543008e79160ea6cd02cb96d
Hasaranga <hasarangawije2345@gmail.com>
2022-05-31 20:06:51 +0530

    hasa push

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,25 @@ const router2 = require("./routes/ResearchRoute");
 const router3 = require("./routes/StudentGroupRoutes");
 
 //pdf
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const fileupload = require("express-fileupload");
+// const fileupload = require("express-fileupload");
 
 dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(cors({ origin:'*'}));
+
+//file upload
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/upload", require("./routes/FileUploadRoutes"));
+
 //database password - 6qb48qJBagUji7gg
+
+//udula DB
 
 // mongoose.connect(
 //   "mongodb+srv://udula:udula123@cluster0.ovykurx.mongodb.net/ResearchTool?retryWrites=true&w=majority",
@@ -25,16 +35,28 @@ app.use(express.json());
 //   }
 // );
 
+  //my DB don't delete
+// mongoose.connect(
+//   "mongodb+srv://admin:6qb48qJBagUji7gg@cluster0.gt2iz.mongodb.net/ResearchTool?retryWrites=true&w=majority",
+//   () => {
+//     console.log("succeessfully connected to the database");
+   
+//   }
+// );
+
 mongoose.connect(
-  "mongodb+srv://admin:6qb48qJBagUji7gg@cluster0.gt2iz.mongodb.net/ResearchTool?retryWrites=true&w=majority",
-  () => {
-    console.log("succeessfully connected to the database");
-  }
-);
+  "mongodb+srv://admin:6qb48qJBagUji7gg@cluster0.gt2iz.mongodb.net/ResearchTool?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=>{console.log("MongoDB connected successfully")
+}).catch((err)=> console.log("DB conncetion failed".err))
+ 
+
+
 
 //pdf
-app.use(cors());
-app.use("/user", require("./routes/pdf"));
+// app.use(cors());
+// app.use("/user", require("./routes/pdf"));
 
 const userRouter = require('./routes/User');
 app.use('/user',userRouter)
@@ -56,4 +78,5 @@ app.use("/studentGroup", router3);
 
 app.listen(5000, () => {
   console.log("server started");
+  console.log("App is Running on port 5000")
 });
